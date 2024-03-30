e2ed520d50eb64c970953da766686bd2e0bd52db
Gustavo <gs.cesario1998@gmail.com>
2024-03-29 22:23:48 -0300

    Rota para criar usuários

---
+++ priv/repo/migrations/20240329194854_add_users_table.exs
@@ -0,0 +1,14 @@
+defmodule BananaBank.Repo.Migrations.AddUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table("users") do
+      add :name, :string, null: false
+      add :password_hash, :string, null: false
+      add :email, :string, null: false, unique: true
+      add :cep, :string, null: false
+
+      timestamps()
+    end
+  end
+end
